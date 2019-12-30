4b6a1c3a4ffb62b7185daefa4f1bee747b1dd7fd
katerynashydlovska <shydlovskayaka@gmail.com>
2019-12-29 21:01:03 -0800

    form for new aplication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Header = (props) =>(
-  <div className="header">
-    <div className="container">
-      <h1 className="header__title">{props.title}</h1>
-    {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
-    </div>
-  </div>
-  );
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Add</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Get Help</NavLink>
 
-Header.defaultProps = {
-  title: 'Indecision'
-}
+  </header>
+);
 
 export default Header;
